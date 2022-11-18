459fc5311e0395df0ba0846c4cc83530260351c1
Rodolfo Campos <rodolfo123@gmail.com>
2022-11-18 13:00:08 -0300

    solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'lists/index'
+  get 'lists/new'
+  get 'lists/show'
+  get 'movies/index'
+  get 'movies/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "movies#index"
+  resources :movies, only: [:index, :show]
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
